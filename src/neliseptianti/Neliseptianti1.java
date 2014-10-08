7b0d466701d177ed973c546f84330de1cebf7390
MYP <sword.arch@yahoo.co.id>
2014-10-08 14:54:42 +0700

    commit

---
+++ src/neliseptianti/Neliseptianti1.java
@@ -0,0 +1,18 @@
+
+package neliseptianti;
+
+
+public class Neliseptianti {
+
+   
+    public static void main(String[] args) {
+       String nama="neli septianti";
+        System.out.println("nama:"+nama);
+        String npm="13311144";
+        System.out.println("npm:"+npm);
+        String kls="si13e";
+        System.out.println("kls:"+kls);
+        String kampus="teknokrat";
+        System.out.println("kampus:"+kampus);
+    }
+}
